39242f114ccbce49bba260ab7863c111ccaa956d
Daniel Boone <danielboone@Daniels-MacBook-Air.local>
2023-01-31 09:41:40 +0700

    remove ref

--- github.js
+++ github.js
@@ -7,7 +7,7 @@ async function getGithubConfigFlags(auth, owner, repo, path) {
     auth,
   });
   return client
-    .request("GET /repos/{owner}/{repo}/contents/{path}{?ref}", {
+    .request("GET /repos/{owner}/{repo}/contents/{path}", {
       owner,
       repo,
       path,
