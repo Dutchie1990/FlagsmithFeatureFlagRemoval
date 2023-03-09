56cedd78e9b3c87c9ce1fc10d4e4a4d3553db7d8
Daniel Boone <danielboone@Daniels-MacBook-Air.local>
2023-03-09 22:51:03 +0700

    add build

--- dist/index.js
+++ dist/index.js
@@ -12838,67 +12838,72 @@ function createMessage(deletedFlags, archivedFlags, ref, dryRun) {
   var message = {};
   var blocks = [];
 
-  let deleteHeaderText =
-    dryRun === false
-      ? `:put_litter_in_its_place: ${deletedFlags.length} Flags has been removed from the ${project}`
-      : `:put_litter_in_its_place: ${deletedFlags.length} Flags ready to be removed from the ${project}`;
+  if (deletedFlags) {
+    let deleteHeaderText =
+      dryRun === false
+        ? `:put_litter_in_its_place: ${deletedFlags.length} Flags has been removed from the ${project}`
+        : `:put_litter_in_its_place: ${deletedFlags.length} Flags ready to be removed from the ${project}`;
 
-  const deleteHeader = {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: deleteHeaderText,
-      emoji: true,
-    },
-  };
-
-  blocks.push(deleteHeader);
-  blocks.push(divider);
-
-  for (let index = 0; index < deletedFlags.length; index++) {
-    const flagName = deletedFlags[index];
-    const teamName = deletedFlags[index].split("-")[0];
-
-    var deleteMessage = {
-      type: "section",
+    const deleteHeader = {
+      type: "header",
       text: {
-        type: "mrkdwn",
-        text: `*${teamName.toUpperCase()}* - ${flagName}`,
+        type: "plain_text",
+        text: deleteHeaderText,
+        emoji: true,
       },
     };
-    blocks.push(deleteMessage);
+
+    blocks.push(deleteHeader);
+    blocks.push(divider);
+
+    for (let index = 0; index < deletedFlags.length; index++) {
+      const flagName = deletedFlags[index];
+      const teamName = deletedFlags[index].split("-")[0];
+
+      var deleteMessage = {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*${teamName.toUpperCase()}* - ${flagName}`,
+        },
+      };
+      blocks.push(deleteMessage);
+    }
   }
 
-  let archiveHeaderText =
-    dryRun === false
-      ? `:file_folder: ${archivedFlags.length} Flags has been archived in the ${project}`
-      : `:file_folder: ${archivedFlags.length} Flags ready to be archived in the ${project}`;
+  if (archivedFlags) {
+    let archiveHeaderText =
+      dryRun === false
+        ? `:file_folder: ${archivedFlags.length} Flags has been archived in the ${project}`
+        : `:file_folder: ${archivedFlags.length} Flags ready to be archived in the ${project}`;
 
-  const archiveHeader = {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: archiveHeaderText,
-      emoji: true,
-    },
-  };
-
-  blocks.push(archiveHeader);
-  blocks.push(divider);
-
-  for (let index = 0; index < archivedFlags.length; index++) {
-    const flagName = archivedFlags[index];
-    const teamName = archivedFlags[index].split("-")[0];
-
-    var archiveMessage = {
-      type: "section",
+    const archiveHeader = {
+      type: "header",
       text: {
-        type: "mrkdwn",
-        text: `*${teamName.toUpperCase()}* - ${flagName}`,
+        type: "plain_text",
+        text: archiveHeaderText,
+        emoji: true,
       },
     };
-    blocks.push(archiveMessage);
+
+    blocks.push(archiveHeader);
+    blocks.push(divider);
+
+    for (let index = 0; index < archivedFlags.length; index++) {
+      const flagName = archivedFlags[index];
+      const teamName = archivedFlags[index].split("-")[0];
+
+      var archiveMessage = {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*${teamName.toUpperCase()}* - ${flagName}`,
+        },
+      };
+      blocks.push(archiveMessage);
+    }
   }
+
   message.blocks = blocks;
   return message;
 }
@@ -17195,7 +17200,9 @@ async function run() {
         );
       }
 
-      await slackAPI.sendMessage(message, slackWebhook);
+      if (message != "") {
+        await slackAPI.sendMessage(message, slackWebhook);
+      }
     }
 
     core.info("Done");
